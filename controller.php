24a0ecd72261a685aa13f2264cb15943f41858b7
Jon Morris <jon.morris@design-portfolio.co.uk>
2021-12-09 20:56:44 +0000

    Bumps package version

--- controller.php
+++ controller.php
@@ -10,7 +10,7 @@ class Controller extends Package {
 
 	protected $pkgHandle = 'multi_page_selector_attribute';
 	protected $appVersionRequired = '5.7.5';
-	protected $pkgVersion = '0.9.1';
+	protected $pkgVersion = '1.0.0';
 	
 	public function getPackageDescription() {
 		return t("Attribute that allows the selection of multiple pages");
